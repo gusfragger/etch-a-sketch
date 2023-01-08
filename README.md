8356a46103d5c061fc8f1b121d0ccfac8221e3ca
Gustavo Pereira <gpereira87@gmail.com>
2023-01-08 02:13:58 +0200

    readme.md added, sync with local files for html, css and js

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# etch-a-sketch
-Etch a sketch game created with HTML, CSS and JS.
+#Etch-a-Sketch 🎨
+Etch-a-Sketch is a simple drawing board that allows users to draw by moving their mouse over the cells on the board. The board can be resized using the range slider and the user can select the drawing mode using the buttons.
+
+![webapp picture](images\etch-a-sketch.png)
+
+##Features 🔥
+*Black and White mode: solid black color with an alpha channel value of FF (fully opaque).
+*RGB mode: random color with a random alpha channel value.
+*Shadows mode: increases the alpha channel value of the current cell color.
+*Lights mode: decreses the alpha channel value of the current cell color.
+*Custom Color mode: allows the user to select a custom color using a color picker.
+
+##How to Use 💻
+1.Open the index.html file in your browser.
+2.Use the range slider to adjust the size of the board.
+3.Click the "APPLY" button to create a new board with the selected size.
+4.Select the drawing mode using the buttons.
+5.Use your mouse to draw on the board.
+
+##Starting from a Mockup 🎨
+I started this project from a moodboard to get an idea of what I wanted, opted for this 70's inspired theme.
+
+![Mockup picture](images\moddboard.jpg)
+
+##Live Preview 🔥
+You can try a live preview of this project at the following 
+link: LIVE PREVIEW
+
+##Future Features 🔮
+*Allow users to download their drawings as pixel art or as icons.
+
+##Technologies Used 💻
+*HTML
+*CSS
+*JavaScript
+
+##Credits 🙏
+This project was created by Gus Pereira.
+
+##License 📜
+This project is licensed under the MIT License. See the LICENSE file for details.
+
+
--- app.js
+++ app.js
@@ -2,46 +2,52 @@ const board = document.querySelector(".container");
 const gridSlider = document.querySelector(".grid-slider");
 const sliderValue = document.querySelector(".slider-value");
 const sizeButton = document.querySelector(".size-button");
-const mode = document.forms[0];
+const colorPicker = document.getElementById('color-picker');
+const bwButton = document.getElementById('bw');
+const rgbButton = document.getElementById('rgb');
+const shadowsButton = document.getElementById('shadows');
+const lightsButton = document.getElementById('lights');
+const pickColorButton = document.getElementById('pick-color');
+const modeButtons= document.querySelectorAll('.inactive');
 let root = document.documentElement;
 const cells = document.querySelectorAll(".cell");
 let isDrawing = false;
+let mode = 'bw';  // default drawing mode black and white
+let customColor= "#6BDBBA";
+
 
 //INITIAL SETUP
 drawBoard(gridSlider.value);
 
-//SLIDER DIV VALUE UPDATE
+// Drawing mode selection
+modeButtons.forEach(function(button) {
+  button.addEventListener('click', function() {
+    // Toggle the active class on the clicked button
+    button.classList.toggle('active');
+    // Remove the active class from the other buttons
+    modeButtons.forEach(function(otherButton) {
+      if (otherButton !== button) {
+        otherButton.classList.remove('active');
+      }
+    });
+    // Set the mode variable to the value of the clicked button
+    mode = button.value;
+  });
+});
+//Sets a custom color when the color picker is used
+colorPicker.addEventListener('input', () => {
+  customColor = colorPicker.value;
+});
+
+//Slider value update
 gridSlider.oninput = () => {
   sliderValue.innerHTML = `${gridSlider.value} x ${gridSlider.value} `;
 };
 
-//USER BUTTONS
-//BOARD CREATION
+//Board clearing and new board creation when user clicks apply
 sizeButton.addEventListener("click", () => {
   eraseBoard();
   drawBoard(gridSlider.value);
-});
-
-cells.forEach((cell) => {
-  cell.addEventListener("mousedown", () => {
-    isDrawing = true;
-  });
-
-  cell.addEventListener("mouseup", () => {
-    isDrawing = false;
-  });
-
-  cell.addEventListener("mouseenter", () => {
-    if (isDrawing) {
-      cell.style.setProperty(
-        "--paint-color",
-        selectionHandling(
-          mode,
-          getComputedStyle(cell).getPropertyValue("--paint-color")
-        )
-      );
-    }
-  });
 });
 
 //FUNCTIONS
@@ -65,7 +71,7 @@ function drawBoard(x) {
       isDrawing = false;
     });
     //Paints the cells according to user selection
-    cell.addEventListener("mouseenter", () => {
+    cell.addEventListener("mouseover", () => {
       if (isDrawing) {
         cell.style.setProperty(
           "--paint-color",
@@ -80,20 +86,38 @@ function drawBoard(x) {
   }
 }
   
-  function selectionHandling(mode, cellColor) {
-    if (mode[0].checked) {
-      // Return a solid black color value with an alpha channel value of FF (fully opaque).
-      return "#000000FF";
-    }
-    if (mode[1].checked) {
-      // Return a random color with a random alpha channel value.
-      return randomHex();
-    }
-    if (mode[2].checked) {
-      // Use the darkener function to increase the alpha channel value of the cellColor argument.
-      return darkener(cellColor, 10);
+function selectionHandling(mode, cellColor) {
+  
+  if (mode === 'bw') {
+    // Return a solid black color value with an alpha channel value of FF (fully opaque).
+    return "#000000FF";
+  }
+  if (mode === 'rgb') {
+    // Return a random color with a random alpha channel value.
+    return randomHex();
+  }
+  if (mode === 'shadows') {
+    // Use the darkener function to increase the alpha channel value of the cellColor argument.
+    if (cellColor === customColor) {
+      return darkener(customColor,10);
+    } else {
+      return darkener(cellColor,10);
     }
   }
+  if (mode === 'lights') {
+    // Return the selected color.
+    if (cellColor === customColor) {
+      return lightener(customColor,10);
+    } else {
+      return lightener(cellColor,10);
+    }
+  }
+  if (mode === 'pick-color') {
+    return customColor;
+  }
+}
+
+  
   
   function randomHex() {
     // Generate a random color value.
@@ -107,16 +131,35 @@ function drawBoard(x) {
   function darkener(color, factor) {
     // Extract the current alpha channel value from the color argument.
     const currentAlpha = color.substr(-2);
-    // Convert the current alpha channel value to a decimal number.
-    const currentAlphaDec = parseInt(currentAlpha, 16);
-    // Increase the current alpha channel value by the factor, but limit it to FF (fully opaque).
-    const newAlphaDec = Math.min(currentAlphaDec + factor, 255);
-    // If the new alpha channel value is fully opaque, return a solid black color value with an alpha channel value of FF (fully opaque).
-    if (newAlphaDec === 255) {
-      return "#000000FF";
-    }
-    // Convert the new alpha channel value to a hexadecimal value.
-    const newAlpha = newAlphaDec.toString(16).toUpperCase();
-    // Return the color argument with the increased alpha channel value.
-    return color.slice(0, -2) + newAlpha;
+    // Convert the alpha channel value to a number.
+    const alpha = parseInt(currentAlpha, 16);
+    // Increase the alpha channel value by the factor.
+    const newAlpha = Math.min(255, alpha + factor);
+    // Convert the new alpha channel value to a hexadecimal string.
+    const newAlphaHex = newAlpha.toString(16).toUpperCase();
+    // Pad the new alpha channel value with zeros if necessary.
+    const paddedNewAlpha = "0".repeat(2 - newAlphaHex.length) + newAlphaHex;
+    // Replace the old alpha channel value with the new alpha channel value.
+    const newColor = color.substr(0, color.length - 2) + paddedNewAlpha;
+    // Return the modified color value.
+    return newColor;
   }
+  
+ 
+
+function lightener(color, factor) {
+  // Extract the current alpha channel value from the color argument.
+  const currentAlpha = color.substr(-2);
+  // Convert the alpha channel value to a number.
+  const alpha = parseInt(currentAlpha, 16);
+  // Decrease the alpha channel value by the factor.
+  const newAlpha = Math.max(0, alpha - factor);
+  // Convert the new alpha channel value to a hexadecimal string.
+  const newAlphaHex = newAlpha.toString(16).toUpperCase();
+  // Pad the new alpha channel value with zeros if necessary.
+  const paddedNewAlpha = "0".repeat(2 - newAlphaHex.length) + newAlphaHex;
+  // Replace the old alpha channel value with the new alpha channel value.
+  const newColor = color.substr(0, color.length - 2) + paddedNewAlpha;
+  // Return the modified color value.
+  return newColor;
+}
